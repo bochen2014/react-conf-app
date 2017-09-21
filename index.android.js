aa96fb7420ad0ee685b1e1c1a6101d6e92b9196a
Bo Chen <bo.chen@kindredgroup.com>
2017-09-21 17:51:59 +1000

    run locally

--- index.android.js
+++ index.android.js
@@ -2,4 +2,5 @@
 import ReactConf2017 from './app/ReactConf2017';
 import { AppRegistry } from 'react-native';
 
+console.disableYellowBox = true;
 AppRegistry.registerComponent('ReactConf2017', () => ReactConf2017);
